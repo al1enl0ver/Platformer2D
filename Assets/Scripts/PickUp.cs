45efe18597d32649fe27bd95d5e137b29ff0475e
bodya <bogdana.dem@gmail.com>
2025-05-31 15:48:29 +0300

    Player, movement

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public float shrimp;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    // void OnCollisionEnter2D(Collision2D other)
+    // {
+    //     if (other.CompareTag ("Player"))
+    //     {
+    //         Destroy(gameObject);
+    //         shrimp++;
+    //     }
+    // }       
+}
+
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    private float Move;
+    private Rigidbody2D rb2d;
+    [SerializeField] public float jump;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(Move * speed, rb2d.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(new Vector2(rb2d.velocity.x, jump));
+        }
+    }
+}
