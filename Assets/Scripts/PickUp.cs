e4df48af36cf33e0b20699935b275b626ee1aa6d
bodya <bogdana.dem@gmail.com>
2025-05-31 21:30:04 +0300

    Score, UI

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,27 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickUp : MonoBehaviour
 {
-    public float shrimp;
+    [SerializeField] private int shrimp = 0;
+    public Text shrimpText;
+
     void Start()
     {
-
+        
     }
 
     void Update()
     {
-
+        shrimpText.text = "Shrimp : " + shrimp.ToString();
     }
 
-    // void OnCollisionEnter2D(Collision2D other)
-    // {
-    //     if (other.CompareTag ("Player"))
-    //     {
-    //         Destroy(gameObject);
-    //         shrimp++;
-    //     }
-    // }       
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Shrimp")
+        {
+            Destroy(other.gameObject);
+            shrimp++;
+        }
+    }       
 }
 
